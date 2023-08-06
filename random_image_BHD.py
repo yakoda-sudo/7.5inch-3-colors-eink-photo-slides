49dc98e8e586549a59b2cc24f86ad2b941308309
yakoda-sudo <85778625+yakoda-sudo@users.noreply.github.com>
2023-08-06 23:29:23 +0100

    7.5_880X528_3_color_module
    epd7in5b_HD module 7.5 inch R/B/W 3 color 880X528

---
+++ random_image_BHD.py
@@ -0,0 +1,69 @@
+##
+##display for 7.5 inch R/B/W 3 color eink to display better quality, need to use my script to convert picture to 3 colors 800x480
+##
+##
+from PIL import Image
+from PIL import ImageDraw
+import time
+import os
+import random
+from waveshare_epd import epd7in5b_HD
+import numpy
+
+EPD_WIDTH = 880
+EPD_HEIGHT = 528
+
+#def choose_random_loading_image(path):
+#    images=os.listdir(path)
+#    loading_image=random.randint(0,len(images)-1)
+#   return path+images[loading_image]
+# define the picture directory
+picdir = '/home/stevek/E-Paper-PhotoSlideShow/bmp/'
+
+def main():
+    epd = epd7in5b_HD.EPD()
+    epd.init()
+#    epd.Clear()
+    # For simplicity, the arguments are explicit numerical coordinates
+    imageB1 = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 255)    # 1: clear the frame
+#    ImageDraw.Draw(imageB)
+    imageR1 = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 255)
+#    ImageDraw.Draw(imageR)
+#    localimg = choose_random_loading_image('bmp/')
+    localimg = random.choice(os.listdir('bmp/'))
+    imageB1 = Image.open(picdir+localimg).convert('RGB')
+    imageR1 = Image.open(picdir+localimg).convert('RGB')
+    pixelB = imageB1.load()
+    pixelR = imageR1.load()
+    for i in range(imageB1.size[0]):
+        for j in range(imageB1.size[1]):
+            if pixelB[i, j] != (0, 0, 0):
+                imageB1.putpixel((i, j), (255, 255, 255))
+            else:
+                pass
+    for m in range(imageR1.size[0]):
+        for n in range(imageR1.size[1]):
+            if pixelR[m, n] != (255, 0, 0):
+                imageR1.putpixel((m, n), (255, 255, 255))
+            if pixelR[m, n] == (255, 0, 0):
+                imageR1.putpixel((m, n), (0, 0, 0))
+            else:
+                pass
+#    h, w = image.size
+#    if h < w:
+#        image = image.rotate(270, expand=True)
+#    else:
+#        pass
+#    resized_img = image.resize((EPD_WIDTH, EPD_HEIGHT))
+    imageB1 = imageB1.convert('1', dither=Image.FLOYDSTEINBERG)
+    imageR1 = imageR1.convert('1', dither=Image.FLOYDSTEINBERG)
+#    imageB1.show()
+#    imageR1.show()
+    epd.display(epd.getbuffer(imageB1), epd.getbuffer(imageR1))
+#    time.sleep(3600)  # change the image every minute
+    exit()
+    main()
+
+
+if __name__ == '__main__':
+    main()
